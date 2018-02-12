c9d55fe86f4f5089b826984ccdfca0c18524475d
karntrehan <karntrehan@gmail.com>
2018-02-12 18:42:19 +0530

    [ENH] ListLocalData tested

--- posts/src/main/java/com/karntrehan/posts/commons/data/local/UserDao.kt
+++ posts/src/main/java/com/karntrehan/posts/commons/data/local/UserDao.kt
@@ -3,9 +3,14 @@ package com.karntrehan.posts.commons.data.local
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import io.reactivex.Flowable
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun upsertAll(users: List<User>)
+
+    @Query("SELECT * FROM user")
+    fun getAll(): Flowable<List<User>>
 }
